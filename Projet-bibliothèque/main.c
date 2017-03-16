1eba6f6b06aabb16651bec96ead28fc19fb5a549
Paul-Henri Zimmerlin <Paul-Henri@client.openvpn.net>
2017-03-16 17:19:39 +0100

    Ajout de la deuxième dimension en itératif

--- Projet-bibliothèque/main.c
+++ Projet-bibliothèque/main.c
@@ -31,14 +31,31 @@ void affichageTab1D(char* tab, int taille){
     }
 }
 
-char** creerTab2D(int taille, int tailley){
-    return '**';
+char** creerTab2D(int taillex, int tailley){
+    char** x = malloc(taillex * sizeof(char*));
+    for(int i = 0; i < taillex; i++){
+        *(x+i)=creerTab1D(tailley);
+    }
+    return x;
+}
+void init2D(char **x, int taillex, tailley){
+    
+    
+}
+void affichageTab2D(char** tab, int taillex, int tailley){
+    for(int i = 0; i < taillex; i++){
+        for(int j = 0; j < tailley; j++){
+            printf("%c ", *(*(tab + i) + j));
+        }
+        printf("\n");
+    }
 }
 
 
 int main(int argc, const char * argv[]) {
     
-    affichageTab1D(creerTab1D(10), 10);
+    //affichageTab1D(creerTab1D(10), 10);
+    affichageTab2D(creerTab2D(4, 5), 4, 5);
     return 0;
 
 }
