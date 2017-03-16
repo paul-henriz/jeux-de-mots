e7dac533ad69e9064bc67258b379b50761b287a3
Paul-Henri Zimmerlin <Paul-Henri@client.openvpn.net>
2017-03-16 16:56:10 +0100

    Ajout de la fonction récursive pour l'affichage 1D

--- Projet-bibliothèque/main.c
+++ Projet-bibliothèque/main.c
@@ -7,10 +7,38 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+char* creerTab1D(int taille);
+void initTab1D(char* tab, int taille);
+void affichageTab1D(char* tab, int taille);
+
+
+char* creerTab1D(int taille){
+    char* tab = malloc(taille * sizeof(char));
+    initTab1D(tab, taille);
+    return tab;
+}
+void initTab1D(char* tab, int taille){
+    if(taille == -1) return;
+    *(tab + taille - 1) = '*';
+    initTab1D(tab, --taille);
+}
+void affichageTab1D(char* tab, int taille){
+    if(*tab){
+        printf("%c ", *tab);
+        affichageTab1D(++tab, taille);
+    }
+}
+
+char** creerTab2D(int taille, int tailley){
+    return '**';
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    affichageTab1D(creerTab1D(10), 10);
     return 0;
 
 }
