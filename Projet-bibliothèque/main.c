dbab9c9504dc4cee20874f5401b302f3cdf64010
Paul-Henri Zimmerlin <Paul-Henri@macbook-pro-de-paul-henri.local>
2017-03-30 22:41:52 +0200

    Redimensionnement automatique du tableau lors d'insertion de mots ou de caractères en dehors

--- Projet-bibliothèque/main.c
+++ Projet-bibliothèque/main.c
@@ -17,7 +17,6 @@ char** creerTab2D(int taillex, int tailley);
 void init2D(char **x, int taillex, int tailley);
 void affichageTab2D(char** tab, int taillex, int tailley);
 
-
 char* creerTab1D(int taille){
     char* tab = malloc(taille * sizeof(char));
     initTab1D(tab, taille);
@@ -61,17 +60,34 @@ void AffichageTab2D(char **tab, int ligne, int colonne, int i, int j){
     }
 }
 
-void insererCharDansTab2D(char **tab, int taillex, int tailley, int posx, int posy, char c){
+void insererCharDansTab2D(char ***tab, int *taillex, int *tailley, int posx, int posy, char c){
     if(posx >= 0 && posy >= 0){
-        if(posx < taillex && posy < tailley){
-            tab[posx][posy] = c;
+        
+        if (posx + 1 > *taillex) {
+            *tab = realloc(*tab, (posx + 1) * sizeof(char **));
+            
+            for(int i = *taillex; i < posx + 1; i++){
+                (*tab)[i] = creerTab1D(*tailley);
+            }
+            
+            *taillex = posx + 1;
         }
-        else if(DEBUG) printf("\nLa position n'est pas correcte (à l'extérieur du tableau)\n");
+        if (posy + 1 > *tailley){
+            for(int ligne = 0; ligne < *taillex; ligne++){
+                (*tab)[ligne] = realloc((*tab)[ligne], (posy + 1) * sizeof(char *));
+                for(int colonne = *tailley; colonne < (posy + 1); colonne++){
+                    (*tab)[ligne][colonne] = '*';
+                }
+            }
+            *tailley = posy + 1;
+        }
+        
+        (*tab)[posx][posy] = c;
     }
     else if(DEBUG) printf("\nLa position n'est pas correcte (inférieure à 0)\n");
 }
 
-void insererMotDansTab2D(char **tab, int taillex, int tailley, int posx, int posy, int horizontal, char *mot){
+void insererMotDansTab2D(char ***tab, int *taillex, int *tailley, int posx, int posy, int horizontal, char *mot){
     while(*mot){
         insererCharDansTab2D(tab, taillex, tailley, posx, posy, *mot);
         mot++;
@@ -82,18 +98,26 @@ void insererMotDansTab2D(char **tab, int taillex, int tailley, int posx, int pos
 }
 
 
+char accesTad2D(char **tab, int taillex, int tailley, int posx, int posy){
+    if(posx < (taillex - 1) && posy < (tailley - 1)) return *(*(tab + posx) + posy);
+    else return '*';
+}
+
 
 
 int main(int argc, const char * argv[]) {
-    
+    int taillex = 5;
+    int tailley = 5;
     //affichageTab1D(creerTab1D(4), 4);
-    char** test = creerTab2D(4, 5);
-    AffichageTab2D(test, 4, 5, 0, 0);
-    //insererCharDansTab2D(test, 4, 5, 2, 2, 'c');
-    //AffichageTab2D(test, 4, 5, 0, 0);
+    char** test = creerTab2D(taillex, tailley);
+    
+    AffichageTab2D(test, taillex, tailley, 0, 0);
     char* machin = "coucou";
-    insererMotDansTab2D(test, 4, 5, 2, 2, 0, machin);
-    AffichageTab2D(test, 4, 5, 0, 0);
+    printf("\n\n");
+    insererMotDansTab2D(&test, &taillex, &tailley, 2, 2, 0, machin);
+    
+    AffichageTab2D(test, taillex, tailley, 0, 0);
+    //printf("Test : %c\n", accesTad2D(test, 4, 5, 2, 2));
 
     return 0;
     
